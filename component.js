0083894be796941e522fe463971d3a5b0e38cc0c
Tsubouchi55 <ryg.tsubouchi@gmail.com>
2020-11-12 21:34:51 +0900

    テーブルをコンポーネントで表示

--- component.js
+++ component.js
@@ -1,3 +1,36 @@
-Vue.component('flag-text', {
-  
+Vue.component('user-item', {
+  props: ['user'],
+  template: '<li>{{ user.name }}</li>'
 })
+
+new Vue({
+  el: "#app-user",
+  data: {
+    users: [
+      {id: 001, name: "AAA"},
+      {id: 002, name: "BBB"},
+      {id: 003, name: "CCC"},
+    ]
+  }
+});
+
+Vue.component('user-table', {
+  props: ['user'],
+  template: '\
+    <tr>\
+      <td>{{ user.id }}</td>\
+      <td>{{ user.name }}</td>\
+    </tr>\
+    '
+})
+
+new Vue({
+  el: "#app-table",
+  data: {
+    users: [
+      {id: 001, name: "ABC"},
+      {id: 002, name: "DEF"},
+      {id: 003, name: "GHI"},
+    ]
+  }
+});
