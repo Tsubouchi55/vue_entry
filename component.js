e8ef0a1ed15c734f2bb8439ddc9e9c703db525c9
Tsubouchi55 <ryg.tsubouchi@gmail.com>
2020-11-15 23:10:58 +0900

    doAddメソッド追加

--- component.js
+++ component.js
@@ -1,11 +1,11 @@
 Vue.component('user-table', {
   props: ['user'],
-  template: '\
-    <tr>\
-      <td>{{ user.id }}</td>\
-      <td>{{ user.name }}</td>\
-    </tr>\
-    '
+  template: `
+    <tr>
+      <td>{{ user.id }}</td>
+      <td>{{ user.name }}</td>
+    </tr>
+    `
 })
 
 var apptable = new Vue({
@@ -21,36 +21,51 @@ var apptable = new Vue({
 
 Vue.component('input-form', {
   props: ['form'],
-  template: '\
-  <form>\
-    <input type="text" ref="newName">\
-    <button v-on:click="doAdd">Add</button>\
-  </form>\
-    '
+  template: `
+  <form>
+    <input type="text" ref="newName">
+    <button v-on:click="doAdd">Add</button>
+  </form>
+  `,
+  methods: {
+    doAdd: function() {
+      var newName = this.$refs.newName.value;
+      var userLen = apptable.users.length + 1;
+      if (!newName.length) {
+        return;
+      }
+      if (!apptable.users.some((e) => e.name == newName)) {
+        apptable.users.push ({
+          id: (000 + userLen).slice(-3),
+          name: newName
+        });
+      }
+    }
+  }
 });
 //メソッドを後から追加しているからダメっぽい
 //コンポーネントにメソッドを追加できないか検証
 var appform = new Vue({
   el: "#app-form",
 
-  methods: {
-    doAdd: function() {
-      var newName = this.$refs.newName;
-      var usersLen = apptable.users.length + 1;
-      if (!newName.value.length) {
-        return;
-      }
-      if (!apptable.users.some((e) => e.name == newName.value)) {
-        apptable.users.push({
-          id: ("000" + usersLen).slice(-3),
-          name: newName.value,
-        });
-        // this.error = false;
-        newName.value = "";
-      }
-      // } else {
-      //   this.error = true;
-      // }
-    },
-  }
+  // methods: {
+  //   doAdd: function() {
+  //     var newName = this.$refs.newName;
+  //     var usersLen = apptable.users.length + 1;
+  //     if (!newName.value.length) {
+  //       return;
+  //     }
+  //     if (!apptable.users.some((e) => e.name == newName.value)) {
+  //       apptable.users.push({
+  //         id: ("000" + usersLen).slice(-3),
+  //         name: newName.value,
+  //       });
+  //       // this.error = false;
+  //       newName.value = "";
+  //     }
+  //     // } else {
+  //     //   this.error = true;
+  //     // }
+  //   },
+  // }
 })
